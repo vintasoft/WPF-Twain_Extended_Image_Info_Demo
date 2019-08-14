5d6187a1ec1e3d319e51d60408ab98c6e19c73d8
unknown <D:\The Bat!>
2019-08-14 11:42:34 +0300

    Source codes update

--- CSharp/Properties/AssemblyInfo.cs
+++ CSharp/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("VintaSoft Ltd.")]
 [assembly: AssemblyProduct("WPF Twain Extended Image Info Demo")]
-[assembly: AssemblyCopyright("Copyright VintaSoft Ltd. 2018")]
+[assembly: AssemblyCopyright("Copyright VintaSoft Ltd. 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("10.3.1.3")]
-[assembly: AssemblyFileVersion("10.3.1.3")]
+[assembly: AssemblyVersion("10.3.8.1")]
+[assembly: AssemblyFileVersion("10.3.8.1")]
