71e994b8aaef21ed40153d925cd7c93765d51427
vintasoft <D:\The Bat!>
2020-12-23 17:12:45 +0300

    Source codes update

--- CSharp/App.xaml.cs
+++ CSharp/App.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.ComponentModel;
-using System.IO;
 using System.Windows;
 
 namespace WpfTwainExtendedImageInfoDemo
@@ -16,8 +15,6 @@ namespace WpfTwainExtendedImageInfoDemo
         public App()
             : base()
         {
-            VintasoftTwain.VintasoftWpfTwainLicense.Register();
-
             AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
         }
 
@@ -34,20 +31,11 @@ namespace WpfTwainExtendedImageInfoDemo
                 // show information about licensing exception
                 MessageBox.Show(string.Format("{0}: {1}", licenseException.GetType().Name, licenseException.Message), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
 
-                string[] dirs = new string[] { ".", "..", @"..\..\", @"..\..\..\", @"..\..\..\..\..\", @"..\..\..\..\..\..\..\" };
-                // for each directory
-                for (int i = 0; i < dirs.Length; i++)
-                {
-                    string filename = System.IO.Path.Combine(dirs[i], "VSTwainNetEvaluationLicenseManager.exe");
-                    // if VintaSoft Evaluation License Manager exists in directory
-                    if (System.IO.File.Exists(filename))
-                    {
-                        // start Vintasoft Evaluation License Manager for getting the evaluation license
-                        System.Diagnostics.Process process = new System.Diagnostics.Process();
-                        process.StartInfo.FileName = filename;
-                        process.Start();
-                    }
-                }
+                // open article with information about usage of evaluation license
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo.FileName = "https://www.vintasoft.com/docs/vstwain-dotnet/Licensing-Twain-Evaluation.html";
+                process.StartInfo.UseShellExecute = true;
+                process.Start();
             }
         }
 
@@ -67,5 +55,6 @@ namespace WpfTwainExtendedImageInfoDemo
                 return GetLicenseException(ex.InnerException);
             return null;
         }
+
     }
 }
